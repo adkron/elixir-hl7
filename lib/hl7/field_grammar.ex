dbb005624926d1bb852310b1f1260a59867daeaf
Amos King <amos.king@hcahealthcare.com>
2024-03-21 12:42:02 -0500

    Sharing to see if we want to go this direction.
    This could be used to parse paths.

--- lib/hl7/field_grammar.ex
+++ lib/hl7/field_grammar.ex
@@ -2,21 +2,21 @@ defmodule Parser do
   @moduledoc """
   An HL7 Path is defined as:
 
-SEG[SEG_NUM]-F[REPEAT_NUM][[.C].S]
+  SEG[SEG_NUM]-F[REPEAT_NUM][[.C].S]
 
-where:
+  where:
 
-SEG – a 3 character segment name. For example PID.
+  SEG – a 3 character segment name. For example PID.
 
-[SEG_NUM] – SEG_NUM is an integer value where SEG_NUM>0 or SEG_NUM='*'. If [SEG_NUM] is omitted, SEG[1] is assumed. If [SEG_NUM]='*', all matching segments are included in the search. For example, OBR[2] means the second OBR segment, whereas OBR[1] and OBR are equivalent.
+  [SEG_NUM] – SEG_NUM is an integer value where SEG_NUM>0 or SEG_NUM='*'. If [SEG_NUM] is omitted, SEG[1] is assumed. If [SEG_NUM]='*', all matching segments are included in the search. For example, OBR[2] means the second OBR segment, whereas OBR[1] and OBR are equivalent.
 
-F – is an integer value where F>0. For example, PID-3 means the 3rd field in the PID segment.
+  F – is an integer value where F>0. For example, PID-3 means the 3rd field in the PID segment.
 
-[REPEAT_NUM]– is the Field Repeat number where REPEAT_NUM>0 or REPEAT_NUM='*'. If REPEAT_NUM='*' all repeating fields are included in the query. If [REPEAT_NUM] is omitted, F[1] is assumed.
+  [REPEAT_NUM]– is the Field Repeat number where REPEAT_NUM>0 or REPEAT_NUM='*'. If REPEAT_NUM='*' all repeating fields are included in the query. If [REPEAT_NUM] is omitted, F[1] is assumed.
 
-C – C>0 or is absent. If C is absent, all components in the field are included separated by '^'. For example, PID-3 means PID-3.1 +PID-3.2+PID-3.3...PID-3.N.
+  C – C>0 or is absent. If C is absent, all components in the field are included separated by '^'. For example, PID-3 means PID-3.1 +PID-3.2+PID-3.3...PID-3.N.
 
-S – S>0 or is absent. If S is absent, all subcomponents in the component are included separated by '&'. If S is omitted, S=1 is assumed. For example, PID-3.2.3 means the third subcomponent of the component of PID-3, and PID-3.2 means PID-3.2.1+PID-3.2.2+PID-3.2.3...PID-3.2.N.
+  S – S>0 or is absent. If S is absent, all subcomponents in the component are included separated by '&'. If S is omitted, S=1 is assumed. For example, PID-3.2.3 means the third subcomponent of the component of PID-3, and PID-3.2 means PID-3.2.1+PID-3.2.2+PID-3.2.3...PID-3.2.N.
   """
   import NimbleParsec
 
